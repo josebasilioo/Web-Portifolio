5952628205ec931db7e3fe0f6a5230f966acef82
josebasilioo <josebasiliosilvaneto@gmail.com>
2023-05-15 21:24:59 -0300

    feature: project-base

---
+++ src/components/TopBar/topbar.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import styles from './topbar.module.css'
+
+const options = [
+    {
+        name: 'Home',
+        href: '/#home'
+    },
+    {
+        name: 'Expertise',
+        href: '/#expertise'
+    },
+    {
+        name: 'Projetos',
+        href: '/#projects'
+    },
+    {
+        name: 'Experiências',
+        href: '/#experiences'
+    },
+    {
+        name: 'Contato',
+        href: '/#contact'
+    },
+]
+
+const TopBar: React.FC = () => {
+  return (
+    <section className={styles.topbar}>
+      {options.map((element) => (
+        <a key={element.href} className={styles.sections} href={element.href}>{element.name}</a>
+      ))
+
+      }
+    </section>
+  );
+}
+
+export default TopBar;
