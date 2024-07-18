195238926c1a14202cca4db20d04ad0d2b9f8c5a
josebasilioo <josebasiliosilvaneto@gmail.com>
2024-07-17 22:15:12 -0300

    fixed delay speed

--- src/views/Main/Main.tsx
+++ src/views/Main/Main.tsx
@@ -196,7 +196,7 @@ export default function Main() {
     setIsClient(true); // Set the flag indicating that we are on the client
 
     const showTypewritersInSequence = async () => {
-      const delays = content.map((_, i) => i * 0);
+      const delays = content.map((_, i) => i * 10);
 
       for (let i = 0; i < delays.length; i++) {
         await delay(delays[i]);
